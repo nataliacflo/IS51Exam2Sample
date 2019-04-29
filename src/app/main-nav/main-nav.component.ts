e12c374718cfb3524f548b78508add8fa2c8848e
Natalia Flores <nataliaflores@swuser-129-8-197-89.swlan.csufresno.edu>
2019-04-29 16:02:44 -0700

    finish exam

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from '../toast/toast.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastService: ToastService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  showAbout() {
+    this.toastService.showToast('success', 5000, 'This application was created by Natalia Flores (c).');
+  }
+
+  logout() {
+    localStorage.setItem('user', JSON.stringify({}));
+    this.router.navigate(['login']);
   }
 
 }
